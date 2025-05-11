325638663436ee0a9593c4e98e92fd22f9fa364f
Pedro Augusto Ribeiro Gomes <pedroaurgomes@Pedros-MacBook-Pro.local>
2025-05-11 19:42:09 -0300

    feat(db) database initialization session working

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     frontend_url: str
+    database_url: str
     
     class Config:
         env_file = ".env"
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
 from app.core.config import settings
+from app.core.db import create_db_and_tables
 
-app = FastAPI()
+# for now we dont need the "async" because there are no async operations on startup or shutdown
+# However, when scaling the app we will definitely have some, so we are future-proofing it.
+@asynccontextmanager 
+async def lifespan(app: FastAPI):
+    create_db_and_tables() # sync operation but okay to be inside async since it is really light
+    yield
+    
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
@@ -12,7 +21,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# endpoint for health check page
+# Health check endpoint (simple /ping)
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
