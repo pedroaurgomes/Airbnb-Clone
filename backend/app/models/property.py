e15c33b159cb80dc22b84b7906647d4eb9cdad88
Pedro Augusto Ribeiro Gomes <pedroaurgomes@Pedros-MacBook-Pro.local>
2025-05-13 18:45:09 -0300

    fix(models): profissionalizing tables names: plural (e.g. properties instead of property)

--- backend/app/models/property.py
+++ backend/app/models/property.py
@@ -1,11 +1,12 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from typing import List
 
 class Property(SQLModel, table=True):
-    # __tablename__ = "properties"
+    __tablename__ = "properties"
 
     property_id: Optional[int] = Field(default=None, primary_key=True)
-    host_id: int = Field(foreign_key="user.user_id")
+    host_id: int = Field(foreign_key="users.user_id")
     title: str
     address: str
     city: str
