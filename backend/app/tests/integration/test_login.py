afe24d216a633467f9c947f7bf479562160a22ed
Pedro Augusto Ribeiro Gomes <pedroaurgomes@Pedros-MacBook-Pro.local>
2025-05-13 18:47:35 -0300

    test(update): updating login endpoint tests to JWT

--- backend/app/tests/integration/test_login.py
+++ backend/app/tests/integration/test_login.py
@@ -4,52 +4,52 @@ from app.core.security import hash_password
 from app.models.user import User
 from app.core.db import get_session
 from sqlmodel import Session
+from app.tests.utils.factories import create_test_user
 
 client = TestClient(app)
 
-# Helper function to create a user manually inside DB
-def create_test_user(session: Session, email: str, password: str):
-    user = User(
-        name="Test User",
-        email=email,
-        hashed_password=hash_password(password),
-        role="guest"
-    )
-    session.add(user)
-    session.commit()
-    session.refresh(user)
-    return user
-
 def test_login_success():
     with next(get_session()) as session:
-        create_test_user(session, "loginsuccess@example.com", "strongpassword123")
+        create_test_user(session, "loginsuccess@example.com", "strongpassword123", "guest")
 
-    response = client.post("/v1/users/login", json={
-        "email": "loginsuccess@example.com",
-        "password": "strongpassword123"
-    })
+    response = client.post(
+        "/v1/users/login",
+        data={
+            "username": "loginsuccess@example.com",
+            "password": "strongpassword123"
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"})
+
     assert response.status_code == 200
     data = response.json()
-    assert data["email"] == "loginsuccess@example.com"
-    assert "user_id" in data
+    assert "access_token" in data
+    assert data["token_type"] == "bearer"
 
 def test_login_wrong_password():
     with next(get_session()) as session:
-        create_test_user(session, "wrongpassword@example.com", "rightpassword123")
+        create_test_user(session, "wrongpassword@example.com", "rightpassword123", "guest")
 
-    response = client.post("/v1/users/login", json={
-        "email": "wrongpassword@example.com",
-        "password": "wrongpassword123"
-    })
-    assert response.status_code == 400
+    response = client.post(
+        "/v1/users/login", 
+        data={
+            "username": "wrongpassword@example.com",
+            "password": "wrongpassword123"
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"},)
+
+    assert response.status_code == 401 # unauthorized
     data = response.json()
     assert data["detail"] == "Invalid email or password."
 
 def test_login_non_existent_email():
-    response = client.post("/v1/users/login", json={
-        "email": "doesnotexist@example.com",
-        "password": "any_password"
-    })
-    assert response.status_code == 400
+    response = client.post(
+        "/v1/users/login", 
+        data={
+            "username": "doesnotexist@example.com",
+            "password": "any_password"
+        },
+        headers={"Content-Type": "application/x-www-form-urlencoded"})
+
+    assert response.status_code == 401 # unauthorized
     data = response.json()
     assert data["detail"] == "Invalid email or password."
